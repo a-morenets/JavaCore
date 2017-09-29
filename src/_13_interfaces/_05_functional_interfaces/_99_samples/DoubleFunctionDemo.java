86c6833f0be021c6f9ba3e5a6b49250fb624236e
Oleksii Morenets <alexey.morenets@gmail.com>
2017-09-29 17:19:55 +0300

    flatMap

--- src/_13_interfaces/_05_functional_interfaces/_99_samples/DoubleFunctionDemo.java
+++ src/_13_interfaces/_05_functional_interfaces/_99_samples/DoubleFunctionDemo.java
@@ -1,6 +1,9 @@
 package _13_interfaces._05_functional_interfaces._99_samples;
 
+import java.util.Arrays;
 import java.util.function.DoubleFunction;
+import java.util.stream.DoubleStream;
+import java.util.stream.Stream;
 
 public class DoubleFunctionDemo {
 
@@ -8,7 +11,11 @@ public class DoubleFunctionDemo {
         double[] arr = {5.2, 1.0, 3.1};
 
         //		DoubleFunction<R> doubleFunction;
-        DoubleFunction doubleFunction;
+        DoubleFunction<DoubleStream> doubleFunction = value -> DoubleStream.of(arr);
+
+        Arrays.stream(arr)
+                .flatMap(doubleFunction)
+                .forEach(value -> System.out.println(value + ": "));
     }
 
 }
