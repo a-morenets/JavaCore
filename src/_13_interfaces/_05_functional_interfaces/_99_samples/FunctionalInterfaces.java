ed610b102c0c144e9ca80d5d517a85ed3204744f
Oleksii Morenets <alexey.morenets@gmail.com>
2017-10-02 22:00:15 +0300

    FunctionDemo

--- src/_13_interfaces/_05_functional_interfaces/_99_samples/FunctionalInterfaces.java
+++ src/_13_interfaces/_05_functional_interfaces/_99_samples/FunctionalInterfaces.java
@@ -3,10 +3,6 @@ package _13_interfaces._05_functional_interfaces._99_samples;
 public class FunctionalInterfaces {
 
 	public static void main(String[] args) {
-//		ObjIntConsumer<T> objIntConsumer;
-//		ObjLongConsumer<T> objLongConsumer;
-//		Predicate<T> predicate;
-//		Supplier<T> supplier;
 //		ToDoubleBiFunction<T, U> toDoubleBiFunction;
 //		ToDoubleFunction<T> toDoubleFunction;
 //		ToIntBiFunction<T, U> toIntBiFunction;
